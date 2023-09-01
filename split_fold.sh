7804539512871dcb090fde11be54a3743b2f8eb1
jl4mc <jl4mc@orchid.rnet.missouri.edu>
2023-09-01 16:33:21 -0500

    updated scripts

--- split_fold.sh
+++ split_fold.sh
@@ -4,8 +4,8 @@
 
 #python gate/network/edge_directed_kmeans_node/split_fold.py --datadir dataset/CASP15_scores/sample/kmeans_v3_c2/ --scoredir dataset/CASP15_scores/ --outdir dataset/CASP15_scores/processed_dataset_directed_kmeans_v3_c2_sim/0.3 --sim_threshold 0.3
 
-python gate/network/edge_directed_kmeans_node/split_fold.py --datadir dataset/CASP15_scores/sample/kmeans_v2_c2/ --scoredir dataset/CASP15_scores/ --outdir dataset/CASP15_scores/processed_dataset_directed_kmeans_v2_c3_sim/0 --sim_threshold 0
+python gate/network/edge_directed_kmeans_node/split_fold.py --datadir dataset/CASP15_scores/sample/kmeans_v2_c3/ --scoredir dataset/CASP15_scores/ --outdir dataset/CASP15_scores/processed_dataset_directed_kmeans_v2_c3_sim/0 --sim_threshold 0
 
-python gate/network/edge_directed_kmeans_node/split_fold.py --datadir dataset/CASP15_scores/sample/kmeans_v2_c2/ --scoredir dataset/CASP15_scores/ --outdir dataset/CASP15_scores/processed_dataset_directed_kmeans_v2_c2_sim/0 --sim_threshold 0
+#python gate/network/edge_directed_kmeans_node/split_fold.py --datadir dataset/CASP15_scores/sample/kmeans_v2_c2/ --scoredir dataset/CASP15_scores/ --outdir dataset/CASP15_scores/processed_dataset_directed_kmeans_v2_c2_sim/0 --sim_threshold 0
 
-python gate/network/edge_directed_kmeans_node/split_fold.py --datadir dataset/CASP15_scores/sample/kmeans_v3_c2/ --scoredir dataset/CASP15_scores/ --outdir dataset/CASP15_scores/processed_dataset_directed_kmeans_v3_c2_sim/0 --sim_threshold 0
+#python gate/network/edge_directed_kmeans_node/split_fold.py --datadir dataset/CASP15_scores/sample/kmeans_v3_c2/ --scoredir dataset/CASP15_scores/ --outdir dataset/CASP15_scores/processed_dataset_directed_kmeans_v3_c2_sim/0 --sim_threshold 0
