f14461bf00a5370c183af76a34dd19094cf53a9f
jl4mc <jl4mc@orchid.rnet.missouri.edu>
2023-12-21 21:14:08 -0600

    updated training scripts

---
+++ slurm/directed_kmeans_v6_t2000_sim0.5/hell_gpu/fold6.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#-------------------------------------------------------------------------------
+#  SBATCH CONFIG
+#-------------------------------------------------------------------------------
+## resources
+#SBATCH --partition gpu
+#SBATCH --ntasks-per-node=16  # cores per task
+#SBATCH --mem=50G  # memory per core (default is 1GB/core)
+#SBATCH --time 2-00:00     # days-hours:minutes time 
+#SBATCH --gres gpu:A100
+#SBATCH --job-name=directed_kmeans_v6_t2000_sim0.5_fold6
+#SBATCH --output=directed_kmeans_v6_t2000_sim0.5_fold6-%j.out  # %j is the unique jobID
+
+module load cuda/11.8.0
+
+source /home/jl4mc/data/mambaforge/bin/activate
+
+conda activate gate
+
+export PYTHONPATH=/home/jl4mc/data/gate/
+
+cd /home/jl4mc/data/gate/
+
+python gate/network/edge_directed_kmeans_node/train_single_fold_seed_v3_mango.py --datadir dataset/CASP15_scores/sample/kmeans_v6/kmeans_sil_t2000/ --scoredir dataset/CASP15_scores/ --outdir dataset/CASP15_scores/processed_dataset_directed_kmeans_v6/0.5/kmeans_sil_t2000/ --project directed_kmeans_v6_t2000_sim0.5 --dbdir experiments/ --labeldir dataset/CASP15_scores/label/ --log_val_mse true --fold 6
