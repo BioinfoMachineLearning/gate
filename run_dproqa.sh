3a7b6483ac9e65bbef6afc5a36bb27972385ab16
Jian Liu <709501841@qq.com>
2024-03-23 21:07:53 -0500

    Delete run_dproqa.sh

--- run_dproqa.sh
+++
@@ -1,3 +0,0 @@
-python gate/feature/generate_dproq_scores.py --indir /home/jl4mc/gate/dataset/CASP15_inhouse/server_dataset/models_filtered --outdir /home/jl4mc/gate/dataset/CASP15_inhouse/server_dataset/dproqa/ --interface_dir /home/jl4mc/gate/dataset/CASP15_inhouse/server_dataset/interface_model_size/
-
-python gate/feature/generate_dproq_scores.py --indir /home/jl4mc/gate/dataset/CASP15_inhouse/human_dataset/models_filtered --outdir /home/jl4mc/gate/dataset/CASP15_inhouse/human_dataset/dproqa/ --interface_dir /home/jl4mc/gate/dataset/CASP15_inhouse/human_dataset/interface_model_size/
